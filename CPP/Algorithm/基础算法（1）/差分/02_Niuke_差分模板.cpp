e57e771a72a6a38507221761b3db7bcc8a055130
Yu-dai <d2025440304@gmail.com>
2026-04-01 15:25:00 +0800

    差分

---
+++ CPP/Algorithm/基础算法（1）/差分/02_Niuke_差分模板.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+typedef long long LL;
+const int N = 1e6;
+LL f[N];
+int n,m;
+int main()
+{
+    cin >> n >> m;
+    for(int i = 1;i<=n;i++)
+    {
+        int x;
+        f[i] += x;
+        f[i+1] -= x;
+    }
+
+    while(m--)
+    {
+        int l,r,k; cin >> l >> r >> k;
+        f[l] += k;
+        f[r+1] -= k;
+    }
+
+    for(int i = 1;i<=n;i++)
+    {
+        f[i] = f[i-1] +f[i];
+        cout << f[i] << " ";
+    }
+    return 0;
+}
